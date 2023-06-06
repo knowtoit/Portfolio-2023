2f41890b6645519640ec5eec53463b4c211ea039
Aishwarya <saichaudharid@gmail.com>
2023-06-06 22:47:59 +0530

    First commit

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,40 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com/in/aishwarya-chaudhari-979250194/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://twitter.com/Sai_3196"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="twitter-link" src="../assets/twitter.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com/sai.chaudhari.92"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com/sai_2205/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="instagram-link" src="../assets/instagram.png" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
